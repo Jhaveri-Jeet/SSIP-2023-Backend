bd6a4d3c90a965458a033c2c8cb72bd5d627567c
JeetJhaveri <jeetwork941@gmail.com>
2023-11-24 01:53:42 +0530

    court api modified

--- Features/Courts/CourtsController.cs
+++ Features/Courts/CourtsController.cs
@@ -21,13 +21,13 @@ namespace CriminalDatabaseBackend.Features.Courts
         public async Task<IActionResult> AddCourts([FromBody] Courts court)
         {
             var state = await databaseContext.States.FirstOrDefaultAsync(s => s.Id == court.StateId);
-            if (state == null) { return NotFound(); }
+            if (state == null) return NotFound("States not found");
 
             var district = await databaseContext.Districts.FirstOrDefaultAsync(s => s.Id == court.DistrictId);
-            if (district == null) { return NotFound(); }
+            if (district == null) return NotFound("Districts not found");
 
             var role = await databaseContext.Roles.FirstOrDefaultAsync(s => s.Id == court.RoleId);
-            if (role == null) { return NotFound(); }
+            if (role == null) return NotFound("Roles not found");
 
             await databaseContext.AddAsync(court);
             await databaseContext.SaveChangesAsync();
@@ -39,7 +39,8 @@ namespace CriminalDatabaseBackend.Features.Courts
         public async Task<IActionResult> GetAll()
         {
             var courts = await databaseContext.Courts.Include(courts => courts.State).Include(courts => courts.District).Include(courts => courts.Role).ToListAsync();
-            if (courts == null) return NotFound();
+            if (courts == null) return NotFound("Courts not found");
+
             return Ok(courts);
         }
 
@@ -47,7 +48,7 @@ namespace CriminalDatabaseBackend.Features.Courts
         public async Task<IActionResult> GetById([FromRoute] int id)
         {
             var court = await databaseContext.Courts.Include(court => court.State).Include(court => court.District).Include(courts => courts.Role).FirstOrDefaultAsync(c => c.Id == id);
-            if (court == null) return NotFound();
+            if (court == null) return NotFound("Courts not found");
 
             return Ok(court);
         }
@@ -56,7 +57,7 @@ namespace CriminalDatabaseBackend.Features.Courts
         public async Task<IActionResult> UpdateCourt([FromBody] Courts court, [FromRoute] int id)
         {
             var oldCourt = await databaseContext.Courts.FirstOrDefaultAsync(s => s.Id == id);
-            if (oldCourt == null) { return NotFound(); }
+            if (oldCourt == null) return NotFound("Courts not found");
 
             oldCourt.Name = court.Name;
             oldCourt.IdentificationNumber = court.IdentificationNumber;
